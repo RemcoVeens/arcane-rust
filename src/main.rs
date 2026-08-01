215c4ff92fcb74ac69c3003d89d94334b94db492
remco <remcoveens03@gmail.com>
2026-08-01 16:33:54 +0200

    quest 8 done. next is project

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,57 @@
+use std::println;
+
+const MAX_MANA: u32 = 10_000;
+const RESTORE_RATE: u32 =25;
+const SPELL_NAMES: &str = "Fireball, Frostbolt, Arcane Blast";
+const XP_PER_LEVEL:u32 = 10;
+
 fn main() {
-    let mut mana = 0;
-    while mana < 100 {
-        println!("Meditating... {mana} mana");
-        mana += 25;
-    }
-    println!("Fully restored!\n");
+    println!("=== Constants ===");
+    println!("Max mana: {MAX_MANA}");
+    println!("Restore rate: {RESTORE_RATE} mana/tick");
+    println!("Known spells: {SPELL_NAMES}");
+    
+    println!("\n=== Shadowing (type change) ===");
+    let mana = 100;
+    println!("Mana as number: {mana}");
+    let mana = format!("{mana}/100");
+    println!("Mana as display: {mana}");
 
-    for level in 1..=5 {
-        let spells_unlocked = level * 2;
-        println!("Level {level}: {spells_unlocked} spells available");
-    }
-    println!();
-
-    let spellbook = ["Fireball", "Frostbolt", "Arcane Blast", "Heal", "Shield"];
-    println!("=== Spellbool ===");
-    for spell in spellbook {
-        println!("  - {spell}");
-    }
-    println!();
-
-    for n in (1..=10).rev() {
-        println!("{n} ");
-    }
-    println!("Liftoff!");
-
-    println!();
-    for i in 1..=5 {
-        for j in 1..=5 {
-            let sum = i * j;
-            if sum < 10 {
-                print!("  {sum}");
-            } else {
-                print!(" {sum}");
-            }
+    println!("\n== Shadowing (block scope) ===");
+    let x = 1;
+    {
+        let x =2;
+        println!("Inner x: {x}");
+        {
+            let x =3;
+            println!("Deepest x: {x}");
         }
-        println!()
+        println!("Inner x again: {x}");
     }
+    println!("Outer x: {x}");
 
-    println!();
-    let mut i = 1;
-    loop {
-        if i % 7 == 0 && i % 13 == 0 {
-            break;
-        }
-        i += 1;
+    println!("\n=== Shadowing (transformation) ===");
+    let spell = "  fireball  ";
+    let spell = spell.trim();
+    let spell = spell.to_uppercase();
+    println!("Ready to cast: {spell}");
+
+    println!("\n=== chalanges ===");
+    let xp_till_ten = XP_PER_LEVEL * 10;
+    println!("XP needed to reach level 10: {xp_till_ten} xp");
+
+    let num = "42";
+    let num:u32 = num.parse().expect("not a number");
+    let num = num*2;
+
+    println!("num = {num}");
+
+    let outer:&str = "hellow world";
+    println!("outer: {outer}");
+    {
+        let outer:u32 = 67;
+        println!("Inner: {outer}");
     }
-    println!("{i} is the first number divisible by both 7 and 13");
+    println!("outer: {outer}");
+
 }
